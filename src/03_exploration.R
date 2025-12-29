6bc6a33f7bdd68456fabbcac07b36037b693af79
Yanis Dermoun <yanisdermoun@MacBook-Pro-de-Yanis.local>
2025-12-29 04:17:31 -0500

    analyse déscriptive de la variable réponse

--- src/03_exploration.R
+++ src/03_exploration.R
@@ -2,3 +2,12 @@
 # (Moyenne, médiane, écart-type, min, max)
 
 summary(analyse_assurance)
+
+
+# Étant donné que la variable réponse sera le cout, nous allons plus détailler:
+
+mean(analyse_assurance$couts)
+median(analyse_assurance$couts)
+sd(analyse_assurance$couts)
+min(analyse_assurance$couts)
+max(analyse_assurance$couts)
