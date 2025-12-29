e2d517635fddae219a71d3da98b3b2941cb34bdc
Yanis Dermoun <yanisdermoun@MacBook-Pro-de-Yanis.local>
2025-12-29 06:05:22 -0500

    Mise en place du premier modele GLM

--- src/04_modelisation.R
+++ src/04_modelisation.R
@@ -1,1 +1,13 @@
 # Modélisation actuarielle
+
+# Les données train et test ont déja été set dans le RScript 02_nettoyage
+
+# Modèle glm
+
+modele_glm <- glm(
+  couts~ age + imc + enfant + sexe + fumeur + region,
+  data = train,
+  family = gaussian()
+)
+
+summary(modele_glm)
