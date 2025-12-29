cb69a40268f09eff8e0bfd06eb8cab8e5ea50b70
Yanis Dermoun <yanisdermoun@MacBook-Pro-de-Yanis.local>
2025-12-28 21:53:23 -0500

    renommer les variables afin de faciliter l'analyse (Erreur)

--- src/02_nettoyage.R
+++ src/02_nettoyage.R
@@ -1,5 +1,19 @@
 # Library qu'on aura besoin:
+
 install.packages("dplyr")
 library(dplyr)
 install.packages("tidyverse")
 library(tidyverse)
+
+
+# Renommer les variables:
+assurance <- assurance %>% 
+    rename(
+      age=age,
+      sexe=sex
+      imc=bmi,
+      enfant=children,
+      fumeur=smoker,
+      region=region,
+      couts=charges
+    )
